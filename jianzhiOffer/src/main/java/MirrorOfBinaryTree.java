adaf82a0ac419b123e5bbd4450d7b032b3db855f
YanbingXu <605224031@qq.com>
2019-09-02 20:05:24 +0800

    剑指0ffer增加二叉树镜像

--- jianzhiOffer/src/main/java/MirrorOfBinaryTree.java
+++ jianzhiOffer/src/main/java/MirrorOfBinaryTree.java
@@ -16,7 +16,34 @@
  *
  */
 public class MirrorOfBinaryTree {
+    /**
+     * 二叉树的镜像
+     * @param root
+     */
     public void Mirror(TreeNode root) {
+        if(root!=null){
+            TreeNode left = root.left;
+            root.left= root.right;
+            root.right = left;
+            Mirror(root.left);
+            Mirror(root.right);
+
+        }
+
+
+    }
+
+    /**
+     * 二叉树的建树
+     * @param array 输入数组
+     */
+    public void createBinTree(int[] array){
+
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{8,6,10,5,7,9,11};
+
 
     }
 }
