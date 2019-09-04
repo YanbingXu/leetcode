835feecf778ef3104798c826adcf5a05e99ce31d
YanbingXu <605224031@qq.com>
2019-09-04 21:32:08 +0800

    增加剑指offer，在stack中找出最小值，复杂度O（1）

---
+++ jianzhiOffer/src/main/java/StackIncludeMin.java
@@ -0,0 +1,45 @@
+import java.util.Stack;
+
+/**
+ * 定义栈的数据结构，请在该类型中实现一个能够得到栈中所含最小元素的min函数（时间复杂度应为O（1））。
+ * 思路：空间换时间
+ */
+public class StackIncludeMin {
+    private Stack<Integer> data = new Stack<>();
+    private Stack<Integer> min = new Stack<>();
+    public void push(int node) {
+        if(min.isEmpty() || node<=min.peek() ){
+            min.push(node);
+        }else{
+            min.push(min.peek());
+        }
+        data.push(node);
+    }
+
+    public void pop() {
+        data.pop();
+        min.pop();
+
+    }
+
+    public int top() {
+
+        return data.peek();
+    }
+
+    public int min() {
+        return  min.peek();
+
+    }
+
+    public static void main(String[] args) {
+        StackIncludeMin s = new StackIncludeMin();
+        s.push(3);
+        int min = s.min();
+        System.out.println(min);
+        s.push(4);
+        int min2 = s.min();
+        System.out.println(min2);
+
+    }
+}
