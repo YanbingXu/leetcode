e77d36aa4c86c75e983f931ab928a374111e6124
YanbingXu <605224031@qq.com>
2020-06-07 14:52:20 +0800

    扩展课后习题最后一道，当出现多个最大子串情况时的代码逻辑

--- basic/src/main/java/CommonUsedClass/StringAfterClassTest3.java
+++ basic/src/main/java/CommonUsedClass/StringAfterClassTest3.java
@@ -2,6 +2,8 @@ package CommonUsedClass;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class StringAfterClassTest3 {
     /*
     获取两个字符串中最大相同子串，比如：
@@ -35,13 +37,58 @@ public class StringAfterClassTest3 {
 
     }
 
+    /*
+    对上面的题目进行扩展，如果有多个相同子串，应该怎么做？
+    1。返回类型为String[],先不用arraylist，用stringbuffer转换为String[]
+     */
+    public String[] getMaxSameStrings(String str1, String str2){
+        String maxString = (str1.length()>=str2.length())?str1:str2;
+        String minString = (str1.length()<str2.length())?str1:str2;
+
+        //完善一下鲁棒性
+        if(maxString!=null && minString!=null){
+
+            int length = minString.length();
+            //用一个sb来暂时接受string数组
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i <=length ; i++) {
+                for(int x=0,y=length-i;y<=length;x++,y++){
+                    if(maxString.contains(minString.substring(x,y))){
+                        sb.append(minString.substring(x,y)+",");
+                    }
+
+                }
+
+//                System.out.println(sb);
+                //这里要手动break出来，找到了最大子串，下面的就不找了
+                if(sb.length()>0){
+                    break;
+                }
+            }
+            //debug看的化不用replaceAll好像也可以，split会默认出去最后一个逗号后的空串，但是这里还是加上保证逻辑完整
+            String[] split = sb.toString().replaceAll(",$", "").split("\\,");
+            return split;
+
+        }
+        return null;
+
+
+    }
+
+
     @Test
     public void getMaxSameStringTest(){
-        String str = "abcwerthelloyuiodabcdefgef";
-        String str1 = "cvhellobabcdefgnm";
+        String str = "abcwerthello1yuiodabcdefef";
+        String str1 = "cvhello1babcdefnm";
 
         String maxSameString = getMaxSameString(str, str1);
         System.out.println(maxSameString);
+
+        System.out.println("**********************");
+        String[] maxSameStrings = getMaxSameStrings(str, str1);
+        System.out.println(Arrays.toString(maxSameStrings));
+
 
     }
 }
